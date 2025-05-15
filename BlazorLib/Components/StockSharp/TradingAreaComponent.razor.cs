407553f9da8150abb70cba5f31fe6d0c93efcd9c
Stas Uspenskiy <ru.usa.goo@gmail.com>
2025-05-15 16:37:15 +0300

    ~

---
+++ BlazorLib/Components/StockSharp/TradingAreaComponent.razor.cs
@@ -0,0 +1,13 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman - @FakeGov 
+////////////////////////////////////////////////
+
+namespace BlazorLib.Components.StockSharp;
+
+/// <summary>
+/// TradingAreaComponent
+/// </summary>
+public partial class TradingAreaComponent : BlazorBusyComponentBaseModel
+{
+
+}
