0537b436d5afe9cc40339bf140078242036689b5
Stas Uspenskiy <ru.usa.goo@gmail.com>
2025-05-25 17:26:36 +0300

    ~

--- SharedLibStandart21/Models/StockSharp/InstrumentTradeStockSharpViewModel.cs
+++ SharedLibStandart21/Models/StockSharp/InstrumentTradeStockSharpViewModel.cs
@@ -27,8 +27,33 @@ public partial class InstrumentTradeStockSharpViewModel : InstrumentTradeStockSh
     /// <inheritdoc/>
     public DateTime CreatedAtUTC { get; set; }
 
+    /// <inheritdoc/>
     public void Reload(InstrumentTradeStockSharpViewModel model)
     {
-        throw new NotImplementedException();
+        Board = model.Board;
+        Id = model.Id;
+        IsFavorite = model.IsFavorite;
+        UnderlyingSecurityType = model.UnderlyingSecurityType;
+        UnderlyingSecurityId = model.UnderlyingSecurityId;
+        TypeInstrument = model.TypeInstrument;
+        ShortName = model.ShortName;
+        Shortable = model.Shortable;
+        SettlementType = model.SettlementType;
+        SettlementDate = model.SettlementDate;
+        PrimaryId = model.PrimaryId;
+        OptionType = model.OptionType;
+        OptionStyle = model.OptionStyle;
+        Name = model.Name;
+        Multiplier = model.Multiplier;
+        LastUpdatedAtUTC = model.LastUpdatedAtUTC;
+        IdRemote = model.IdRemote;
+        FaceValue = model.FaceValue;
+        ExpiryDate = model.ExpiryDate;
+        Decimals = model.Decimals;
+        Currency = model.Currency;
+        CreatedAtUTC = model.CreatedAtUTC;
+        Code = model.Code;
+        Class = model.Class;
+        CfiCode = model.CfiCode;
     }
 }
