a607a877702e2793d0ed06fc01d2813b82b252ae
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2025-05-31 17:07:49 +0300

    ~

--- SharedLibStandart21/Models/api/request/TProjectedRequestModel.cs
+++ SharedLibStandart21/Models/api/request/TProjectedRequestModel.cs
@@ -17,5 +17,5 @@ public class TProjectedRequestModel<T>
     /// <summary>
     /// T - Request
     /// </summary>
-    public required T Request { get; set; }
+    public T Request { get; set; }
 }
