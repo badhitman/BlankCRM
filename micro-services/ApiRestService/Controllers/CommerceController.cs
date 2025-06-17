39cfda189d4966d5a36f4116c70440c061fbd985
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2025-06-17 23:27:08 +0300

    Update CommerceController.cs

--- micro-services/ApiRestService/Controllers/CommerceController.cs
+++ micro-services/ApiRestService/Controllers/CommerceController.cs
@@ -19,22 +19,16 @@ public class CommerceController(ICommerceTransmission commRepo) : ControllerBase
     /// <summary>
     /// Подбор номенклатуры (поиск по параметрам)
     /// </summary>
-    /// <remarks>
-    /// Роли: <see cref="ExpressApiRolesEnum.OrdersReadCommerce"/>, <see cref="ExpressApiRolesEnum.OrdersWriteCommerce"/>
-    /// </remarks>
-    [HttpPut($"/api/{Routes.COMMERCE_CONTROLLER_NAME}/{Routes.ORDERS_CONTROLLER_NAME}-{Routes.SELECT_ACTION_NAME}")]
+    [HttpPut($"/api/{Routes.COMMERCE_CONTROLLER_NAME}/{Routes.NOMENCLATURES_CONTROLLER_NAME}-{Routes.SELECT_ACTION_NAME}")]
 #if !DEBUG
     [LoggerNolog]
 #endif
-    public async Task<TPaginationResponseModel<NomenclatureModelDB>> OrdersSelect(TPaginationRequestModel<NomenclaturesSelectRequestModel> req)
+    public async Task<TPaginationResponseModel<NomenclatureModelDB>> NomenclaturesSelect(TPaginationRequestModel<NomenclaturesSelectRequestModel> req)
         => await commRepo.NomenclaturesSelectAsync(req);
 
     /// <summary>
     /// Чтение номенклатуры (по идентификаторам)
     /// </summary>
-    /// <remarks>
-    /// Роли: <see cref="ExpressApiRolesEnum.OrdersReadCommerce"/>, <see cref="ExpressApiRolesEnum.OrdersWriteCommerce"/>
-    /// </remarks>
     [HttpPut($"/api/{Routes.COMMERCE_CONTROLLER_NAME}/{Routes.NOMENCLATURES_CONTROLLER_NAME}-{Routes.READ_ACTION_NAME}")]
 #if !DEBUG
     [LoggerNolog]
