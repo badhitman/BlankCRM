2d0ea2ff2e8149e2d2f4ba2c3ca1ea778629a427
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2025-07-21 21:01:16 +0300

    ~

---
+++ BlazorLib/Components/Rubrics/RubricNodeBodyComponent.razor.cs
@@ -0,0 +1,18 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman - @FakeGov 
+////////////////////////////////////////////////
+
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using SharedLib;
+
+namespace BlazorLib.Components.Rubrics;
+
+public partial class RubricNodeBodyComponent
+{
+    /// <summary>
+    /// Node
+    /// </summary>
+    [Parameter, EditorRequired]
+    public required TreeItemData<UniversalBaseModel?> Node { get; set; }
+}
---
+++ BlazorLib/Components/StockSharp/InstrumentsForRubricComponent.razor.cs
@@ -0,0 +1,12 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman - @FakeGov 
+////////////////////////////////////////////////
+
+using BlazorLib.Components.Rubrics;
+
+namespace BlazorLib.Components.StockSharp;
+
+public partial class InstrumentsForRubricComponent : RubricNodeBodyComponent
+{
+
+}
