3cf839125c5922ed75f6df0690a2878de131dca7
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2025-08-20 15:32:20 +0300

    ~

--- SharedLibStandard21/GlobalStaticConstants/GlobalStaticConstantsCloudStorageMetadataStockSharp.cs
+++ SharedLibStandard21/GlobalStaticConstants/GlobalStaticConstantsCloudStorageMetadataStockSharp.cs
@@ -31,7 +31,7 @@ public static partial class GlobalStaticCloudStorageMetadata
     };
 
     /// <inheritdoc/>
-    public static StorageMetadataModel QuoteVolume => new()
+    public static StorageMetadataModel QuoteStrategyVolume => new()
     {
         ApplicationName = STOCKSHARP_QUOTE_FORM,
         PropertyName = Path.Combine(Routes.QUOTE_CONTROLLER_NAME, Routes.VOLUME_CONTROLLER_NAME),
@@ -39,10 +39,82 @@ public static partial class GlobalStaticCloudStorageMetadata
     };
 
     /// <inheritdoc/>
-    public static StorageMetadataModel QuoteSizeVolume => new()
+    public static StorageMetadataModel QuoteSizeStrategyVolume => new()
     {
         ApplicationName = STOCKSHARP_QUOTE_FORM,
         PropertyName = Path.Combine(Routes.QUOTE_CONTROLLER_NAME, Routes.SIZE_CONTROLLER_NAME, Routes.VOLUME_CONTROLLER_NAME),
         PrefixPropertyName = Routes.DUMP_ACTION_NAME,
+    };
+
+
+    /// <summary>
+    /// QuoteSmallStrategyBidVolume
+    /// </summary>
+    public static StorageMetadataModel QuoteSmallStrategyBidVolume => new()
+    {
+        ApplicationName = Routes.STOCKSHARP_CONTROLLER_NAME,
+        PropertyName = $"{Routes.QUOTE_CONTROLLER_NAME}-{Routes.SMALL_CONTROLLER_NAME}-{Routes.STRATEGY_CONTROLLER_NAME}-{Routes.BID_CONTROLLER_NAME}-{Routes.VOLUME_CONTROLLER_NAME}",
+        PrefixPropertyName = Routes.DEFAULT_CONTROLLER_NAME
+    };
+
+    /// <summary>
+    /// QuoteSmallStrategyOfferVolume
+    /// </summary>
+    public static StorageMetadataModel QuoteSmallStrategyOfferVolume => new()
+    {
+        ApplicationName = Routes.STOCKSHARP_CONTROLLER_NAME,
+        PropertyName = $"{Routes.QUOTE_CONTROLLER_NAME}-{Routes.SMALL_CONTROLLER_NAME}-{Routes.STRATEGY_CONTROLLER_NAME}-{Routes.OFFER_CONTROLLER_NAME}-{Routes.VOLUME_CONTROLLER_NAME}",
+        PrefixPropertyName = Routes.DEFAULT_CONTROLLER_NAME
+    };
+
+    /// <summary>
+    /// ProgramDataPathStockSharp
+    /// </summary>
+    public static StorageMetadataModel ProgramDataPathStockSharp => new()
+    {
+        ApplicationName = Routes.STOCKSHARP_CONTROLLER_NAME,
+        PropertyName = $"{Routes.PROGRAM_CONTROLLER_NAME}-{Routes.DATA_CONTROLLER_NAME}-{Routes.PATH_CONTROLLER_NAME}",
+        PrefixPropertyName = Routes.DEFAULT_CONTROLLER_NAME
+    };
+
+    /// <summary>
+    /// Client code assigned by the broker.
+    /// </summary>
+    public static StorageMetadataModel ClientCodeBrokerStockSharp => new()
+    {
+        ApplicationName = Routes.STOCKSHARP_CONTROLLER_NAME,
+        PropertyName = $"{Routes.CLIENT_CONTROLLER_NAME}-{Routes.CODE_CONTROLLER_NAME}",
+        PrefixPropertyName = Routes.BROKER_CONTROLLER_NAME
+    };
+
+    /// <summary>
+    /// TradeInstrumentStrategyStockSharp
+    /// </summary>
+    public static StorageMetadataModel TradeInstrumentStrategyStockSharp(int ownerId) => new()
+    {
+        ApplicationName = GlobalStaticConstantsTransmission.TransmissionQueues.TradeInstrumentStrategyStockSharpReceive,
+        OwnerPrimaryKey = ownerId,
+        PropertyName = $"{Routes.CLIENT_CONTROLLER_NAME}-{Routes.CODE_CONTROLLER_NAME}",
+        PrefixPropertyName = Routes.BROKER_CONTROLLER_NAME
+    };
+
+    /// <summary>
+    /// SecuritiesCriteriaCodeFilterStockSharp
+    /// </summary>
+    public static StorageMetadataModel SecuritiesCriteriaCodeFilterStockSharp => new()
+    {
+        ApplicationName = Routes.STOCKSHARP_CONTROLLER_NAME,
+        PropertyName = $"{Routes.SECURITIES_CONTROLLER_NAME}-{Routes.CRITERIA_CONTROLLER_NAME}-{Routes.LOOKUP_ACTION_NAME}",
+        PrefixPropertyName = $"{Routes.DEFAULT_CONTROLLER_NAME}-{Routes.CODE_CONTROLLER_NAME}-{Routes.FILTER_CONTROLLER_NAME}"
+    };
+
+    /// <summary>
+    /// BoardCriteriaCodeFilterStockSharp
+    /// </summary>
+    public static StorageMetadataModel BoardCriteriaCodeFilterStockSharp => new()
+    {
+        ApplicationName = Routes.STOCKSHARP_CONTROLLER_NAME,
+        PropertyName = $"{Routes.BOARDS_CONTROLLER_NAME}-{Routes.CRITERIA_CONTROLLER_NAME}-{Routes.LOOKUP_ACTION_NAME}",
+        PrefixPropertyName = $"{Routes.DEFAULT_CONTROLLER_NAME}-{Routes.CODE_CONTROLLER_NAME}-{Routes.FILTER_CONTROLLER_NAME}"
     };
 }
