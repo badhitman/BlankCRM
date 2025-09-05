1218054b8cbd69367280158824c03094bfdb5293
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2025-09-05 19:39:52 +0300

    ~

---
+++ BlazorLib/Components/Telegram/MessagesTableTelegramComponent.razor.cs
@@ -0,0 +1,36 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman - @FakeGov 
+////////////////////////////////////////////////
+
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using SharedLib;
+
+namespace BlazorLib.Components.Telegram;
+
+/// <summary>
+/// MessagesTableTelegramComponent
+/// </summary>
+public partial class MessagesTableTelegramComponent : BlazorBusyComponentBaseModel
+{
+    [Inject]
+    ITelegramBotStandardTransmission TelegramRepo { get; set; } = default!;
+
+
+    /// <summary>
+    /// Here we simulate getting the paged, filtered and ordered data from the server, with a token for canceling this request
+    /// </summary>
+    async Task<TableData<MessageTelegramViewModel>> ServerReload(TableState state, CancellationToken token)
+    {
+        TPaginationRequestStandardModel<SearchMessagesChatModel> req = new()
+        {
+            PageNum = state.Page,
+            PageSize = state.PageSize,
+
+        };
+        TPaginationResponseModel<MessageTelegramViewModel> data = await TelegramRepo.MessagesSelectTelegramAsync(req, token);
+
+        // Return the data
+        return new TableData<MessageTelegramViewModel>() { TotalItems = data.TotalRowsCount, Items = data.Response };
+    }
+}
