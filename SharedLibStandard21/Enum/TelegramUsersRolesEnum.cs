e02d43447c73fec5628a2cc53d45c3f060600be9
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2025-09-05 10:35:04 +0300

    ~

---
+++ SharedLibStandard21/Enum/TelegramUsersRolesEnum.cs
@@ -0,0 +1,17 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman - @FakeGov 
+////////////////////////////////////////////////
+
+namespace SharedLib;
+
+/// <summary>
+/// Roles for TelegramUser`s
+/// </summary>
+public enum TelegramUsersRolesEnum
+{
+    /// <inheritdoc/>
+    Default = 0,
+    
+    /// <inheritdoc/>
+    Admin = 100
+}
