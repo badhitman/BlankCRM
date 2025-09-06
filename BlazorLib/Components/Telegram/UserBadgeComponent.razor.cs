b62adfe27f172fd599c8a2da5fa46a4b3558d9c4
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2025-09-06 17:00:45 +0300

    ~

---
+++ BlazorLib/Components/Telegram/UserBadgeComponent.razor.cs
@@ -0,0 +1,28 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman - @FakeGov 
+////////////////////////////////////////////////
+
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using SharedLib;
+
+namespace BlazorLib.Components.Telegram;
+
+public partial class UserBadgeComponent
+{
+    /// <inheritdoc/>
+    [Parameter, EditorRequired]
+    public required JoinUserChatViewModel JoinUserChat { get; set; }
+
+    /// <inheritdoc/>
+    [Parameter]
+    public string? BadgeColor { get; set; }
+
+
+    private bool _visible;
+    private readonly DialogOptions _dialogOptions = new() { FullWidth = true };
+
+    private void OpenDialog() => _visible = true;
+
+    private void Submit() => _visible = false;
+}
