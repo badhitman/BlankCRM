82e9b8a8725da3adc05b6ea2e9ab46c756b72567
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2025-12-12 20:30:07 +0300

    ~

--- devops/prod-builds.update.sh
+++ devops/prod-builds.update.sh
@@ -1,7 +1,6 @@
 /srv/dumps.sh
 systemctl stop web.app.service comm.app.service tg.app.service api.app.service bus.app.service filesindexing.app.service constructor.app.service hd.app.service identity.app.service kladr.app.service ldap.app.service bank.app.service
 7z a /srv/Cloud.Disk/services-snapshots/all_services_`date +%Y-%m-%d"_"%H_%M_%S`.7z /srv/services/*
-rm -r /srv/services/*
 rsync -av --exclude='appsettings.*' --exclude='nlog.config' /srv/git/builds/ /srv/services/
 chown -R www-data:www-data /srv/services
 chmod -R 777 /srv/services
