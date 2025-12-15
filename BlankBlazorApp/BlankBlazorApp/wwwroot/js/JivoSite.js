e51e38386fc92f35dc813a598361997ab9147bbf
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2025-12-15 16:36:08 +0300

    ~

--- BlankBlazorApp/BlankBlazorApp/wwwroot/js/JivoSite.js
+++ BlankBlazorApp/BlankBlazorApp/wwwroot/js/JivoSite.js
@@ -8,4 +8,32 @@ window.tryInitJivoSite = function () {
         console.log('Widget re active')
     }
 };
-window.addEventListener('change', (event) => { window.tryInitJivoSite(); });
+// window.addEventListener('change', (event) => { window.tryInitJivoSite(); });
+
+(function (history) {
+    var pushState = history.pushState;
+    history.pushState = function (state, title, url) {
+        // Call the original pushState function
+        var ret = pushState.apply(this, arguments);
+        // Dispatch a custom event after the history change
+        window.dispatchEvent(new Event('locationchange'));
+        return ret;
+    };
+    // Also handle replaceState
+    var replaceState = history.replaceState;
+    history.replaceState = function (state, title, url) {
+        var ret = replaceState.apply(this, arguments);
+        window.dispatchEvent(new Event('locationchange'));
+        return ret;
+    };
+    // Listen for the native popstate event and dispatch the custom event
+    window.addEventListener('popstate', function () {
+        window.dispatchEvent(new Event('locationchange'));
+    });
+})(window.history);
+
+// Now you can listen for the custom 'locationchange' event
+window.addEventListener('locationchange', function () {
+    console.log('URL has changed to:', window.location.href);
+    window.tryInitJivoSite();
+});
