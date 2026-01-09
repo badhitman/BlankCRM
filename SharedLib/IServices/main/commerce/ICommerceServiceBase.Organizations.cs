267649fea761d91617d9d5774dd98ef7868a5a65
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2026-01-09 21:24:06 +0300

    ~

---
+++ SharedLib/IServices/main/commerce/ICommerceServiceBase.Organizations.cs
@@ -0,0 +1,67 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman - @FakeGov 
+////////////////////////////////////////////////
+
+namespace SharedLib;
+
+/// <inheritdoc/>
+public partial interface ICommerceServiceBase
+{
+    /// <summary>
+    /// OrganizationOfferContractUpdate
+    /// </summary>
+    public Task<TResponseModel<bool>> OrganizationOfferContractUpdateAsync(TAuthRequestStandardModel<OrganizationOfferToggleModel> req, CancellationToken token = default);
+
+    /// <summary>
+    /// Подбор сотрудников (связи пользователей с компаниями)
+    /// </summary>
+    public Task<TResponseModel<TPaginationResponseStandardModel<UserOrganizationModelDB>>> UsersOrganizationsSelectAsync(TPaginationRequestAuthModel<UsersOrganizationsStatusesRequestModel> req, CancellationToken token = default);
+
+    /// <summary>
+    /// UserOrganizationUpdate
+    /// </summary>
+    public Task<TResponseModel<int>> UserOrganizationUpdateAsync(TAuthRequestStandardModel<UserOrganizationModelDB> req, CancellationToken token = default);
+
+    /// <summary>
+    /// UsersOrganizationsRead
+    /// </summary>
+    public Task<TResponseModel<UserOrganizationModelDB[]>> UsersOrganizationsReadAsync(int[] req, CancellationToken token = default);
+
+    /// <summary>
+    /// Обновление параметров организации. Юридические параметры не меняются, а формируется запрос на изменение, которое должна подтвердить сторонняя система
+    /// </summary>
+    public Task<TResponseModel<int>> OrganizationUpdateAsync(TAuthRequestStandardModel<OrganizationModelDB> req, CancellationToken token = default);
+
+    /// <summary>
+    /// Подбор организаций с параметрами запроса
+    /// </summary>
+    public Task<TPaginationResponseStandardModel<OrganizationModelDB>> OrganizationsSelectAsync(TPaginationRequestAuthModel<OrganizationsSelectRequestModel> req, CancellationToken token = default);
+
+    /// <summary>
+    /// Прочитать данные организаций по их идентификаторам
+    /// </summary>
+    public Task<TResponseModel<OrganizationModelDB[]>> OrganizationsReadAsync(int[] req, CancellationToken token = default);
+
+    /// <summary>
+    /// Обновить/Создать офис/филиал организации
+    /// </summary>
+    public Task<TResponseModel<int>> OfficeOrganizationUpdateAsync(AddressOrganizationBaseModel req, CancellationToken token = default);
+
+    /// <summary>
+    /// Установить реквизиты организации (+ сброс запроса редактирования)
+    /// </summary>
+    /// <remarks>
+    /// Если организация находиться в статусе запроса изменения реквизитов - этот признак обнуляется.
+    /// </remarks>
+    public Task<TResponseModel<bool>> OrganizationSetLegalAsync(OrganizationLegalModel req, CancellationToken token = default);
+
+    /// <summary>
+    /// Прочитать данные адресов организаций по их идентификаторам
+    /// </summary>
+    public Task<TResponseModel<OfficeOrganizationModelDB[]>> OfficesOrganizationsReadAsync(int[] ids, CancellationToken token = default);
+
+    /// <summary>
+    /// Удалить офис/филиал организации
+    /// </summary>
+    public Task<ResponseBaseModel> OfficeOrganizationDeleteAsync(int req, CancellationToken token = default);
+}
--- SharedLib/IServices/main/commerce/ICommerceServiceBase.cs
+++ SharedLib/IServices/main/commerce/ICommerceServiceBase.cs
@@ -150,11 +150,6 @@ public partial interface ICommerceServiceBase
     public Task<ResponseBaseModel> CreateAttendanceRecordsAsync(TAuthRequestStandardModel<CreateAttendanceRequestModel> workSchedules, CancellationToken token = default);
 
     /// <summary>
-    /// OrganizationOfferContractUpdate
-    /// </summary>
-    public Task<TResponseModel<bool>> OrganizationOfferContractUpdateAsync(TAuthRequestStandardModel<OrganizationOfferToggleModel> req, CancellationToken token = default);
-
-    /// <summary>
     /// Недельное расписание обновить/создать
     /// </summary>
     public Task<TResponseModel<int>> WeeklyScheduleUpdateAsync(WeeklyScheduleModelDB work, CancellationToken token = default);
@@ -209,57 +204,4 @@ public partial interface ICommerceServiceBase
     /// Загрузка справочника номенклатуры/офферов
     /// </summary>
     public Task<ResponseBaseModel> UploadOffersAsync(List<NomenclatureScopeModel> req, CancellationToken token = default);
-
-    /// <summary>
-    /// Подбор сотрудников (связи пользователей с компаниями)
-    /// </summary>
-    public Task<TResponseModel<TPaginationResponseStandardModel<UserOrganizationModelDB>>> UsersOrganizationsSelectAsync(TPaginationRequestAuthModel<UsersOrganizationsStatusesRequestModel> req, CancellationToken token = default);
-
-    /// <summary>
-    /// UserOrganizationUpdate
-    /// </summary>
-    public Task<TResponseModel<int>> UserOrganizationUpdateAsync(TAuthRequestStandardModel<UserOrganizationModelDB> req, CancellationToken token = default);
-
-    /// <summary>
-    /// UsersOrganizationsRead
-    /// </summary>
-    public Task<TResponseModel<UserOrganizationModelDB[]>> UsersOrganizationsReadAsync(int[] req, CancellationToken token = default);
-
-    /// <summary>
-    /// Обновление параметров организации. Юридические параметры не меняются, а формируется запрос на изменение, которое должна подтвердить сторонняя система
-    /// </summary>
-    public Task<TResponseModel<int>> OrganizationUpdateAsync(TAuthRequestStandardModel<OrganizationModelDB> req, CancellationToken token = default);
-
-    /// <summary>
-    /// Подбор организаций с параметрами запроса
-    /// </summary>
-    public Task<TPaginationResponseStandardModel<OrganizationModelDB>> OrganizationsSelectAsync(TPaginationRequestAuthModel<OrganizationsSelectRequestModel> req, CancellationToken token = default);
-
-    /// <summary>
-    /// Прочитать данные организаций по их идентификаторам
-    /// </summary>
-    public Task<TResponseModel<OrganizationModelDB[]>> OrganizationsReadAsync(int[] req, CancellationToken token = default);
-
-    /// <summary>
-    /// Обновить/Создать офис/филиал организации
-    /// </summary>
-    public Task<TResponseModel<int>> OfficeOrganizationUpdateAsync(AddressOrganizationBaseModel req, CancellationToken token = default);
-
-    /// <summary>
-    /// Установить реквизиты организации (+ сброс запроса редактирования)
-    /// </summary>
-    /// <remarks>
-    /// Если организация находиться в статусе запроса изменения реквизитов - этот признак обнуляется.
-    /// </remarks>
-    public Task<TResponseModel<bool>> OrganizationSetLegalAsync(OrganizationLegalModel req, CancellationToken token = default);
-
-    /// <summary>
-    /// Прочитать данные адресов организаций по их идентификаторам
-    /// </summary>
-    public Task<TResponseModel<OfficeOrganizationModelDB[]>> OfficesOrganizationsReadAsync(int[] ids, CancellationToken token = default);
-
-    /// <summary>
-    /// Удалить офис/филиал организации
-    /// </summary>
-    public Task<ResponseBaseModel> OfficeOrganizationDeleteAsync(int req, CancellationToken token = default);
 }
