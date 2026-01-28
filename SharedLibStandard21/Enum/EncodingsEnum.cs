92e311fab1b961ff03c94ddbbe6b473cd970a245
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2026-01-28 12:05:50 +0300

    ~

---
+++ SharedLibStandard21/Enum/EncodingsEnum.cs
@@ -0,0 +1,36 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman - @FakeGov 
+////////////////////////////////////////////////
+
+namespace SharedLib;
+
+/// <summary>
+/// EncodingsEnum
+/// </summary>
+public enum EncodingsEnum
+{
+    /// <summary>
+    /// Encoding.UTF8
+    /// </summary>
+    UTF8,
+
+    /// <summary>
+    /// Encoding.ASCII
+    /// </summary>
+    ASCII,
+
+    /// <summary>
+    /// Encoding.Unicode
+    /// </summary>
+    Unicode,
+
+    /// <summary>
+    /// Encoding.BigEndianUnicode
+    /// </summary>
+    BigEndianUnicode,
+
+    /// <summary>
+    /// Encoding.UTF32
+    /// </summary>
+    UTF32,
+}
