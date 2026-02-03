783f20ef8a97f693ffb0d8a2a3eba3727a4c43c7
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2026-02-03 18:02:09 +0300

    ~

---
+++ DBContextLibs/DbPostgreLib/Migrations/20260203150148_MainAppContext009.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbPostgreLib.Migrations
+{
+    /// <inheritdoc />
+    public partial class MainAppContext009 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastReadAtUTC",
+                table: "Dialogs",
+                newName: "LastOnlineAtUTC");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Dialogs_LastReadAtUTC",
+                table: "Dialogs",
+                newName: "IX_Dialogs_LastOnlineAtUTC");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastOnlineAtUTC",
+                table: "Dialogs",
+                newName: "LastReadAtUTC");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Dialogs_LastOnlineAtUTC",
+                table: "Dialogs",
+                newName: "IX_Dialogs_LastReadAtUTC");
+        }
+    }
+}
