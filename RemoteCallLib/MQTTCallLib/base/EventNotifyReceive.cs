2e847d90bc372906301f613c1ff90394242a4d18
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2026-02-10 20:33:34 +0300

    ~

--- RemoteCallLib/MQTTCallLib/base/EventNotifyReceive.cs
+++ RemoteCallLib/MQTTCallLib/base/EventNotifyReceive.cs
@@ -2,50 +2,63 @@
 // © https://github.com/badhitman - @FakeGov 
 ////////////////////////////////////////////////
 
+using static SharedLib.GlobalStaticConstantsRoutes;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using MQTTnet;
-using MQTTnet.Packets;
-using MQTTnet.Protocol;
-using Newtonsoft.Json;
-using SharedLib;
-using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using MQTTnet.Packets;
+using Newtonsoft.Json;
 using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using static SharedLib.GlobalStaticConstantsRoutes;
+using SharedLib;
+using MQTTnet;
+using System;
 
 namespace RemoteCallLib;
 
 /// <summary>
 /// EventNotifyReceive
 /// </summary>
-public class EventNotifyReceive<T>(
-    MQTTClientConfigModel rabbitConf,
-    ILogger<EventNotifyReceive<T>> loggerRepo) : IEventNotifyReceive<T>, IAsyncDisposable
+public class EventNotifyReceive<T> : IEventNotifyReceive<T>, IAsyncDisposable
 {
     /// <summary>
     /// Notify
     /// </summary>
     public event IEventNotifyReceive<T>.AccountHandler? Notify;
 
-    static IMqttClient? mqttClient;
+    IMqttClient? mqttClient;
     MqttClientFactory mqttFactory = new();
-    byte[]? _userInfoBytes;
-    string?
-        LayoutContainerId,
-        queueName;
 
+    readonly MQTTClientConfigModel MQConfigRepo;
+    readonly ILogger<EventNotifyReceive<T>> LoggerRepo;
+    byte[]? _userInfoBytes;
+    string? LayoutContainerId;
     List<KeyValuePair<string, byte[]>>? _propertiesValues;
+    string? queueName;
+
+    /// <summary>
+    /// EventNotifyReceive
+    /// </summary>
+    public EventNotifyReceive(
+        MQTTClientConfigModel rabbitConf,
+        IServiceProvider servicesProvider,
+        ILogger<EventNotifyReceive<T>> loggerRepo)
+    {
+        LoggerRepo = loggerRepo;
+        MQConfigRepo = rabbitConf;
+
+        using IServiceScope scope = servicesProvider.CreateScope();
+    }
 
     /// <inheritdoc/>
     public async Task RegisterAction(string QueueName, Action<T> actNotify, string layoutContainerId, byte[]? userInfoBytes, bool isMute = false, List<KeyValuePair<string, byte[]>>? propertiesValues = null, CancellationToken stoppingToken = default)
     {
-        LayoutContainerId = layoutContainerId;
-        queueName = $"{LayoutContainerId}_{QueueName.Replace("\\", "/")}";
+        queueName = QueueName.Replace("\\", "/");
         _propertiesValues = propertiesValues;
         _userInfoBytes = userInfoBytes;
+        LayoutContainerId = layoutContainerId;
         Task ApplicationMessageReceived(MqttApplicationMessageReceivedEventArgs e)
         {
             string content = Encoding.UTF8.GetString(e.ApplicationMessage.Payload).Trim();
@@ -60,13 +73,13 @@ public class EventNotifyReceive<T>(
             }
             catch (Exception ex)
             {
-                loggerRepo.LogError(ex, $"Ошибка выполнения удалённой команды: {queueName}");
+                LoggerRepo.LogError(ex, $"Ошибка выполнения удалённой команды: {queueName}");
                 //
                 e.ReasonCode = MqttApplicationMessageReceivedReasonCode.UnspecifiedError;
                 e.ResponseReasonString = ex.Message;
             }
             if (sr is null)
-                loggerRepo.LogError($"Ошибка обработки удалённой команды (source is null): {queueName}");
+                LoggerRepo.LogError($"Ошибка обработки удалённой команды (source is null): {queueName}");
             else
             {
                 actNotify(sr);
@@ -75,31 +88,18 @@ public class EventNotifyReceive<T>(
             }
             return Task.CompletedTask;
         }
-
-        if (mqttClient is null)
-        {
-            mqttClient = mqttFactory.CreateMqttClient();
-            await mqttClient.ConnectAsync(GetClientOptionsBuilderMQTT(isMute), stoppingToken);
-        }
-
+        mqttClient = mqttFactory.CreateMqttClient();
         mqttClient.ApplicationMessageReceivedAsync += ApplicationMessageReceived;
-        MqttClientSubscribeOptions subscribeOptions = new()
-        {
-            TopicFilters = [new() { Topic = queueName, QualityOfServiceLevel = MqttQualityOfServiceLevel.AtMostOnce }],
-            UserProperties = [new(Routes.USER_CONTROLLER_NAME, _userInfoBytes ?? Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(null)))]
-        };
-
-        if (_propertiesValues is not null && _propertiesValues.Count != 0)
-            _propertiesValues.ForEach(x => subscribeOptions.UserProperties.Add(new(x.Key, x.Value)));
 
         try
         {
-            await mqttClient.SubscribeAsync(subscribeOptions, cancellationToken: stoppingToken);
-            loggerRepo.LogTrace($"{nameof(queueName)}:{queueName}");
+            await mqttClient.ConnectAsync(GetMqttClientOptionsBuilder(queueName, isMute), stoppingToken);
+            await mqttClient.SubscribeAsync(queueName, cancellationToken: stoppingToken);
+            LoggerRepo.LogTrace($"{nameof(queueName)}:{queueName}");
         }
         catch (Exception ex)
         {
-            loggerRepo.LogError(ex, $"can`t connect/subscribe: `{queueName}`");
+            LoggerRepo.LogError(ex, $"can`t connect/subscribe: `{queueName}`");
             mqttClient.ApplicationMessageReceivedAsync -= ApplicationMessageReceived;
             return;
         }
@@ -108,37 +108,43 @@ public class EventNotifyReceive<T>(
     /// <inheritdoc/>
     public async Task UnregisterAction(bool isMute = false, CancellationToken stoppingToken = default)
     {
-        if (mqttClient is null)
-            return;
-
-        MqttClientUnsubscribeOptionsBuilder unsubscribeOptions = new MqttClientUnsubscribeOptionsBuilder()
-            .WithTopicFilter(queueName);
-
         List<MqttUserProperty> usrProps = [];
-        if (_propertiesValues is not null && _propertiesValues.Count != 0)
-            _propertiesValues.ForEach(x => unsubscribeOptions.WithUserProperty(new(x.Key, x.Value)));
+        if (_propertiesValues is not null)
+            usrProps.AddRange(_propertiesValues.Select(x => new MqttUserProperty(x.Key, x.Value)));
 
-        if (!isMute)
-            unsubscribeOptions.WithUserProperty(new(Routes.USER_CONTROLLER_NAME, _userInfoBytes ?? Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(null))));
-        else
-            unsubscribeOptions.WithUserProperty(new(Routes.MUTE_CONTROLLER_NAME, new ReadOnlyMemory<byte>([1])));
-
-        await mqttClient.UnsubscribeAsync(unsubscribeOptions.Build(), cancellationToken: stoppingToken);
-
+        if (mqttClient?.IsConnected == true)
+        {
+            if (!isMute)
+            {
+                usrProps.Add(new(Routes.USER_CONTROLLER_NAME, _userInfoBytes ?? Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(null))));
+                await mqttClient.DisconnectAsync(new() { UserProperties = usrProps }, stoppingToken);
+            }
+            else
+            {
+                usrProps.Add(new(Routes.MUTE_CONTROLLER_NAME, new ReadOnlyMemory<byte>([1])));
+                await mqttClient.DisconnectAsync(new() { UserProperties = usrProps }, stoppingToken);
+            }
+        }
+        mqttClient?.Dispose();
         Notify = null;
     }
 
-    MqttClientOptions GetClientOptionsBuilderMQTT(bool isMute)
+    MqttClientOptions GetMqttClientOptionsBuilder(string queueName, bool isMute)
     {
         MqttClientOptionsBuilder builder = new MqttClientOptionsBuilder()
-               .WithTcpServer(rabbitConf.Host, rabbitConf.Port)
+               .WithTcpServer(MQConfigRepo.Host, MQConfigRepo.Port)
                .WithProtocolVersion(MQTTnet.Formatter.MqttProtocolVersion.V500)
-               ;
+               .WithClientId($"{queueName} [{nameof(EventNotifyReceive<>)}.{typeof(T).Name}] {Guid.NewGuid()}")
+               .WithUserProperty(Routes.USER_CONTROLLER_NAME, _userInfoBytes ?? Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(null)));
 
-        return new MqttClientOptionsBuilder()
-               .WithTcpServer(rabbitConf.Host, rabbitConf.Port)
-               .WithProtocolVersion(MQTTnet.Formatter.MqttProtocolVersion.V500)
-               .Build();
+        if (isMute)
+            builder.WithUserProperty(Routes.MUTE_CONTROLLER_NAME, new ReadOnlyMemory<byte>([1]));
+
+        if (_propertiesValues is not null)
+            foreach (var userProp in _propertiesValues)
+                builder.WithUserProperty(userProp.Key, new ReadOnlyMemory<byte>(userProp.Value));
+
+        return builder.Build();
     }
 
     /// <inheritdoc/>
