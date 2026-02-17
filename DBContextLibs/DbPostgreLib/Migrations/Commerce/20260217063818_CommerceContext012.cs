8a37a999cb30571b6ad22ba369eefca0c2b0db68
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2026-02-17 09:38:37 +0300

    ~

---
+++ DBContextLibs/DbPostgreLib/Migrations/Commerce/20260217063818_CommerceContext012.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbPostgreLib.Migrations.Commerce
+{
+    /// <inheritdoc />
+    public partial class CommerceContext012 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DeliveryDocumentsRetail_DeliveryType",
+                table: "DeliveryDocumentsRetail");
+
+            migrationBuilder.DropColumn(
+                name: "DeliveryType",
+                table: "DeliveryDocumentsRetail");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DeliveryType",
+                table: "DeliveryDocumentsRetail",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeliveryDocumentsRetail_DeliveryType",
+                table: "DeliveryDocumentsRetail",
+                column: "DeliveryType");
+        }
+    }
+}
