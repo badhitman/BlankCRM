5cf27005c31494a9aa034b797b212a3f6c5a5449
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2026-02-18 10:50:10 +0300

    ~

--- devops/prod-builds.update.sh
+++ devops/prod-builds.update.sh
@@ -1,7 +1,11 @@
 /srv/dumps.sh
 systemctl stop web.app.service comm.app.service tg.app.service api.app.service bus.app.service realtime.app.service filesindexing.app.service constructor.app.service hd.app.service identity.app.service kladr.app.service ldap.app.service bank.app.service
-7z a /srv/Cloud.Disk/services-snapshots/all_services_`date +%Y-%m-%d"_"%H_%M_%S`.7z /srv/services/*
-rsync -av --exclude='appsettings.*' --exclude='nlog.config' /srv/git/builds/ /srv/services/
-chown -R www-data:www-data /srv/services
-chmod -R 777 /srv/services
+7z a /srv/Cloud.Disk/services-snapshots/all_services_`date +%Y-%m-%d"_"%H_%M_%S`.7z /srv/services.prod/*
+find /srv/services.prod/ -type f -name '*.dll' -delete
+find /srv/services.prod/ -type f -name '*.pdb' -delete
+find /srv/services.prod/ -type f -name '*.exe' -delete
+find /srv/services.prod/ -type f -name '*.log' -delete
+rsync -av --exclude='appsettings.*' --exclude='nlog.config' /srv/git/builds/ /srv/services.prod/
+chown -R www-data:www-data /srv/services.prod
+chmod -R 777 /srv/services.prod
 systemctl start comm.app.service web.app.service bus.app.service realtime.app.service filesindexing.app.service tg.app.service api.app.service hd.app.service constructor.app.service identity.app.service kladr.app.service ldap.app.service bank.app.service
