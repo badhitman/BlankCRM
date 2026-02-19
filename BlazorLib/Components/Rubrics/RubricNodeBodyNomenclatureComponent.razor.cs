9667c7f747f928d27acbf876165946fd91e42a96
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2026-02-19 08:03:23 +0300

    ~

--- BlazorLib/Components/Rubrics/RubricNodeBodyNomenclatureComponent.razor.cs
+++ BlazorLib/Components/Rubrics/RubricNodeBodyNomenclatureComponent.razor.cs
@@ -16,30 +16,7 @@ public partial class RubricNodeBodyNomenclatureComponent : RubricNodeBodyCompone
     {
         await SetBusyAsync();
         await base.OnInitializedAsync();
-        await ReloadTags();
-        await SetBusyAsync(false);
-    }
-
-    async Task ReloadTags()
-    {
-        if (Node.Value is null)
-        {
-            SnackBarRepo.Error($"Node.Value is null > `{GetType().FullName}`");
-            return;
-        }
-
-        await SetBusyAsync();
-        TPaginationRequestStandardModel<SelectMetadataRequestModel> req = new()
-        {
-            Payload = new()
-            {
-                ApplicationsNames = string.IsNullOrWhiteSpace(ContextName) ? ["", null] : [ContextName],
-                OwnerPrimaryKey = Node.Value.Id,
-                PropertyName = GlobalStaticConstantsRoutes.Routes.MARKERS_CONTROLLER_NAME,
-                PrefixPropertyName = GlobalStaticConstantsRoutes.Routes.ABOUT_CONTROLLER_NAME,
-            }
-        };
-
+        
         await SetBusyAsync(false);
     }
 }
