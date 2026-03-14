0fdf3ccb1ed750969afe7b48202243cc4a0c0a06
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2026-03-14 13:37:16 +0300

    ~

--- RemoteCallLib/RabbitMQ/Transmission/IdentityTransmissionRabbit.cs
+++ RemoteCallLib/RabbitMQ/Transmission/IdentityTransmissionRabbit.cs
@@ -172,7 +172,7 @@ public class IdentityTransmissionRabbit([FromKeyedServices(nameof(RabbitClient))
     /// <inheritdoc/>
     public async Task<TResponseModel<UserInfoModel[]>> GetUsersOfIdentityAsync(string[] ids_users, CancellationToken token = default)
     {
-        ConcurrentDictionary<string, UserInfoModel?> usersRes = new(ids_users.Distinct().Select(x => new KeyValuePair<string, UserInfoModel?>(x, null)));
+        ConcurrentDictionary<string, UserInfoModel?> usersRes = new(ids_users.Where(x => x != GlobalStaticConstantsRoles.Roles.System).Distinct().Select(x => new KeyValuePair<string, UserInfoModel?>(x, null)));
         List<Task> tasks = [.. ids_users.Select(x => Task.Run(() => { if (cache.TryGetValue($"user-identity/{x}", out UserInfoModel? users_cache) && users_cache is not null) usersRes[x] = users_cache; }))];
         await Task.WhenAll(tasks);
         ids_users = [.. ids_users.Where(x => usersRes[x] is null)];
@@ -193,9 +193,7 @@ public class IdentityTransmissionRabbit([FromKeyedServices(nameof(RabbitClient))
         }
         IQueryable<KeyValuePair<string, UserInfoModel?>> _qn = usersRes.Where(x => x.Value is null).AsQueryable();
         if (_qn.Any())
-        {
             loggerRepo.LogError($"users identity get-error for: {string.Join(", ", _qn.Select(y => y.Key))}");
-        }
 
         return new()
         {
