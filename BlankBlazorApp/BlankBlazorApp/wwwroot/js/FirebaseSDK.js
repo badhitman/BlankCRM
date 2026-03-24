4e62b587609046083b6f384e2da564d7cb682b62
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2026-03-24 13:03:13 +0300

    ~

--- BlankBlazorApp/BlankBlazorApp/wwwroot/js/FirebaseSDK.js
+++ BlankBlazorApp/BlankBlazorApp/wwwroot/js/FirebaseSDK.js
@@ -2,23 +2,11 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/fireba
 import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
 import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-messaging.js";
 
-// Initialize Firebase using the global firebase object exposed by compat libraries
-const firebaseConfig = {
-    apiKey: "AIzaSyCPlUkq609DA2CpFZsP88v-FIfFBU6uGRI",
-    authDomain: "evident-ethos-230204.firebaseapp.com",
-    databaseURL: "https://evident-ethos-230204.firebaseio.com",
-    projectId: "evident-ethos-230204",
-    storageBucket: "evident-ethos-230204.firebasestorage.app",
-    messagingSenderId: "1064563856635",
-    appId: "1:1064563856635:web:5267f1a99da99ef9710c5e",
-    measurementId: "G-HVJ38TKTDN"
-};
-
 window.FirebaseMessagingToken = null;
 window.RealtimeCoreComponent = null;
 window.PublicMessagingToken = null;
 
-const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(window.firebaseConfig);
 const firebaseMessaging = getMessaging(firebaseApp);
 const firebaseAnalytics = getAnalytics(firebaseApp);
 
@@ -35,6 +23,7 @@ window.FirebaseSDK = {
                 // const notification = new Notification("Приветсвую!");
 
                 window.FirebaseMessagingToken = getToken(firebaseMessaging, { vapidKey: window.PublicMessagingToken }).then((currentToken) => {
+                    logEvent(firebaseAnalytics, 'token_received');
                     if (currentToken) {
                         sendTokenToServer(currentToken);
                     } else {
@@ -55,30 +44,6 @@ window.FirebaseSDK = {
     RealtimeRegister: function (dotNetReference) {
         window.RealtimeCoreComponent = dotNetReference;
     }
-}
-
-async function enableNotifications() {
-    // Insert your firebase project config here
-    const firebaseConfig = {
-        apiKey: "AIzaSyCPlUkq609DA2CpFZsP88v-FIfFBU6uGRI",
-        authDomain: "evident-ethos-230204.firebaseapp.com",
-        databaseURL: "https://evident-ethos-230204.firebaseio.com",
-        projectId: "evident-ethos-230204",
-        storageBucket: "evident-ethos-230204.firebasestorage.app",
-        messagingSenderId: "1064563856635",
-        appId: "1:1064563856635:web:5267f1a99da99ef9710c5e",
-        measurementId: "G-HVJ38TKTDN"
-    };
-
-    const app = initializeApp(firebaseConfig);
-    const messaging = getMessaging(app);
-
-    const permission = await Notification.requestPermission();
-    if (permission !== 'granted') {
-        console.log("user denied notifications")
-    }
-
-    const token = await getToken(messaging, { vapidKey: "BHNHODqpqbAdxcZYiEV9Suelf4DsT0mn1MT41P1YkUkCjNNLExbgzGvazLAdweupi3xhOYDwVEzA4gT6G7VCgAU" });
 }
 
 function sendTokenToServer(currentToken) {
