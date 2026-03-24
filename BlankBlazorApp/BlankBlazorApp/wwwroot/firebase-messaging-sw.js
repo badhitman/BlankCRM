93529e8d87ee07e59268183758866fce5d3872f0
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2026-03-24 11:26:43 +0300

    Update firebase-messaging-sw.js

--- BlankBlazorApp/BlankBlazorApp/wwwroot/firebase-messaging-sw.js
+++ BlankBlazorApp/BlankBlazorApp/wwwroot/firebase-messaging-sw.js
@@ -1,5 +1,6 @@
 importScripts("https://www.gstatic.com/firebasejs/12.11.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/12.11.0/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics-compat.js");
 
 // Initialize Firebase using the global firebase object exposed by compat libraries
 const firebaseConfig = {
@@ -13,55 +14,27 @@ const firebaseConfig = {
     measurementId: "G-HVJ38TKTDN"
 };
 
-const app = firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(function (payload) {
-    console.log("[firebase-messaging-sw.js] Received background message ", payload);
-    // ... customize your notification
-});
-
-/*// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
-import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-messaging.js";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCPlUkq609DA2CpFZsP88v-FIfFBU6uGRI",
-    authDomain: "evident-ethos-230204.firebaseapp.com",
-    databaseURL: "https://evident-ethos-230204.firebaseio.com",
-    projectId: "evident-ethos-230204",
-    storageBucket: "evident-ethos-230204.firebasestorage.app",
-    messagingSenderId: "1064563856635",
-    appId: "1:1064563856635:web:5267f1a99da99ef9710c5e",
-    measurementId: "G-HVJ38TKTDN"
-};
-const firebaseApp = initializeApp(firebaseConfig);
-const firebaseMessaging = getMessaging(firebaseApp);
-const firebaseAnalytics = getAnalytics(firebaseApp);
-
 window.FirebaseMessagingToken = null;
 window.RealtimeCoreComponent = null;
 window.PublicMessagingToken = null;
 
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseMessaging = firebase.messaging();
+const firebaseAnalytics = firebase.analytics();
+
+firebaseMessaging.onBackgroundMessage(function (payload) {
+    console.log("[firebase-messaging-sw.js] Received background message ", payload);
+    // ... customize your notification
+});
+
+firebaseMessaging.onMessage(function (payload) {
+    console.log('Message received. ', payload);
+    new Notification(payload.notification.title, payload.notification);
+});
+
 window.FirebaseSDK = {
     Initialize: function (publicMessagingToken) {
-        window.PublicMessagingToken = publicMessagingToken;
-        //firebaseMessaging.onMessage(function (payload) {
-        //    console.log('Message received. ', payload);
-        //    new Notification(payload.notification.title, payload.notification);
-        //});
-
-        //if ("serviceWorker" in navigator) {
-        //    navigator.serviceWorker.register("./firebase-messaging-sw.js").then((registration) => {
-        //        getToken(messaging, { serviceWorkerRegistration: registration, vapidKey: window.PublicMessagingToken });
-        //        // The scope is now determined by the registration
-        //    });
-        //}
-                
+        window.PublicMessagingToken = publicMessagingToken;           
         window.FirebaseSDK.RequestPermission();
     },
     RequestPermission: function () {
@@ -71,18 +44,18 @@ window.FirebaseSDK = {
                 console.info('Notification permission granted.');
                 // const notification = new Notification("Приветсвую!");
 
-                //window.FirebaseMessagingToken = getToken(firebaseMessaging, { vapidKey: window.PublicMessagingToken }).then((currentToken) => {
-                //    if (currentToken) {
-                //        sendTokenToServer(currentToken);
-                //    } else {
-                //        console.warn('No registration token available. Request permission to generate one.');
-                //        setTokenSentToServer(false);
-                //    }
-                //}).catch((err) => {
-                //    console.warn('An error occurred while retrieving token. ', err);
-                //    logEvent(firebaseAnalytics, JSON.stringify(err));
-                //    setTokenSentToServer(false);
-                //});
+                window.FirebaseMessagingToken = getToken(firebaseMessaging, { vapidKey: window.PublicMessagingToken }).then((currentToken) => {
+                    if (currentToken) {
+                        sendTokenToServer(currentToken);
+                    } else {
+                        console.warn('No registration token available. Request permission to generate one.');
+                        setTokenSentToServer(false);
+                    }
+                }).catch((err) => {
+                    console.warn('An error occurred while retrieving token. ', err);
+                    logEvent(firebaseAnalytics, JSON.stringify(err));
+                    setTokenSentToServer(false);
+                });
             }
         })
         .catch(function (err) {
@@ -131,4 +104,4 @@ self.addEventListener('notificationclick', function (event) {
 
         return clients.openWindow(target);
     }));
-});*/
+});
