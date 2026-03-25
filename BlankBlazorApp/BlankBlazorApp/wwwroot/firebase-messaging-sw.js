9ff5d2ef4d7c79e8854bd2ee02f368e4cc10f166
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2026-03-25 10:34:22 +0300

    ~

--- BlankBlazorApp/BlankBlazorApp/wwwroot/firebase-messaging-sw.js
+++ BlankBlazorApp/BlankBlazorApp/wwwroot/firebase-messaging-sw.js
@@ -1,9 +1,20 @@
 importScripts("https://www.gstatic.com/firebasejs/12.11.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/12.11.0/firebase-messaging-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics-compat.js");
-import('./js/FirebaseConfigSDK.js');
 
-const firebaseApp = firebase.initializeApp(window.firebaseConfig);
+// Initialize Firebase using the global firebase object exposed by compat libraries
+const firebaseConfig = {
+    apiKey: "AIzaSyCPlUkq609DA2CpFZsP88v-FIfFBU6uGRI",
+    authDomain: "evident-ethos-230204.firebaseapp.com",
+    databaseURL: "https://evident-ethos-230204.firebaseio.com",
+    projectId: "evident-ethos-230204",
+    storageBucket: "evident-ethos-230204.firebasestorage.app",
+    messagingSenderId: "1064563856635",
+    appId: "1:1064563856635:web:5267f1a99da99ef9710c5e",
+    measurementId: "G-HVJ38TKTDN"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 const firebaseMessaging = firebase.messaging();
 
 firebaseMessaging.onBackgroundMessage(function (payload) {
--- BlankBlazorApp/BlankBlazorApp/wwwroot/js/FirebaseSDK.js
+++ BlankBlazorApp/BlankBlazorApp/wwwroot/js/FirebaseSDK.js
@@ -1,13 +1,24 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
 import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
 import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-messaging.js";
-import('./FirebaseConfigSDK.js');
+
+// Initialize Firebase using the global firebase object exposed by compat libraries
+const firebaseConfig = {
+    apiKey: "AIzaSyCPlUkq609DA2CpFZsP88v-FIfFBU6uGRI",
+    authDomain: "evident-ethos-230204.firebaseapp.com",
+    databaseURL: "https://evident-ethos-230204.firebaseio.com",
+    projectId: "evident-ethos-230204",
+    storageBucket: "evident-ethos-230204.firebasestorage.app",
+    messagingSenderId: "1064563856635",
+    appId: "1:1064563856635:web:5267f1a99da99ef9710c5e",
+    measurementId: "G-HVJ38TKTDN"
+};
 
 window.FirebaseMessagingToken = null;
 window.RealtimeCoreComponent = null;
 window.PublicMessagingToken = null;
 
-const firebaseApp = initializeApp(window.firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 const firebaseMessaging = getMessaging(firebaseApp);
 const firebaseAnalytics = getAnalytics(firebaseApp);
 
