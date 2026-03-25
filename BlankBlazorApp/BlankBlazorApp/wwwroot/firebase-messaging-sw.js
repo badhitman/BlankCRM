7a9b2a30901d22e0813959cae1b03639029499f3
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2026-03-25 09:43:48 +0300

    ~

--- BlankBlazorApp/BlankBlazorApp/wwwroot/firebase-messaging-sw.js
+++ BlankBlazorApp/BlankBlazorApp/wwwroot/firebase-messaging-sw.js
@@ -1,18 +1,7 @@
 importScripts("https://www.gstatic.com/firebasejs/12.11.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/12.11.0/firebase-messaging-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics-compat.js");
-
-// Initialize Firebase using the global firebase object exposed by compat libraries
-const firebaseConfig = {
-    apiKey: "AIzaSyCPlUkq609DA2CpFZsP88v-FIfFBU6uGRI",
-    authDomain: "evident-ethos-230204.firebaseapp.com",
-    databaseURL: "https://evident-ethos-230204.firebaseio.com",
-    projectId: "evident-ethos-230204",
-    storageBucket: "evident-ethos-230204.firebasestorage.app",
-    messagingSenderId: "1064563856635",
-    appId: "1:1064563856635:web:5267f1a99da99ef9710c5e",
-    measurementId: "G-HVJ38TKTDN"
-};
+importScripts("./js/FirebaseConfigSDK.js");
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const firebaseMessaging = firebase.messaging();
---
+++ BlankBlazorApp/BlankBlazorApp/wwwroot/js/FirebaseConfigSDK.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCPlUkq609DA2CpFZsP88v-FIfFBU6uGRI",
+    authDomain: "evident-ethos-230204.firebaseapp.com",
+    databaseURL: "https://evident-ethos-230204.firebaseio.com",
+    projectId: "evident-ethos-230204",
+    storageBucket: "evident-ethos-230204.firebasestorage.app",
+    messagingSenderId: "1064563856635",
+    appId: "1:1064563856635:web:5267f1a99da99ef9710c5e",
+    measurementId: "G-HVJ38TKTDN"
+};
+export { firebaseConfig };
