00ac886709827e03e5b797b150b4f3c6aa96a006
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2026-03-26 11:07:11 +0300

    Update firebase-messaging-sw.js

--- BlankBlazorApp/BlankBlazorApp/firebase-messaging-sw.js
+++ BlankBlazorApp/BlankBlazorApp/firebase-messaging-sw.js
@@ -1,7 +1,6 @@
 importScripts("https://www.gstatic.com/firebasejs/12.11.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/12.11.0/firebase-messaging-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics-compat.js");
-importScripts("/lib/jquery/jquery.js");
 
 // Initialize Firebase using the global firebase object exposed by compat libraries
 const firebaseConfig = {
@@ -20,17 +19,33 @@ const firebaseMessaging = firebase.messaging();
 
 firebaseMessaging.onBackgroundMessage(function (payload) {
     console.log("[firebase-messaging-sw.js] Received background message ", payload);
-    $.post("/firebase/onBackgroundMessage", {
-        messagePayload: payload
+    //$.post("/firebase/onBackgroundMessage", {
+    //    messagePayload: payload
+    //});
+    const response = await fetch("/firebase/onBackgroundMessage", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
     });
     new Notification(payload.notification.title, payload.notification);
 });
 
 firebaseMessaging.onMessage(function (payload) {
     console.log('Message received. ', payload);
-    $.post("/firebase/onMessage", {
-        messagePayload: payload
+    //$.post("/firebase/onMessage", {
+    //    messagePayload: payload
+    //});
+
+    const response = await fetch("/firebase/onMessage", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
     });
+
     navigator.serviceWorker.register('messaging-sw.js');
     navigator.serviceWorker.ready.then(function (registration) {
         payload.notification.data = payload.notification;
