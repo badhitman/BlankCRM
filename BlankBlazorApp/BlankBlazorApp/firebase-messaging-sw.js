de5a2d1f1320da12e1334678f9adf971e24b4980
Успенский Станислав Александрович <ru.usa.goo@gmail.com>
2026-03-28 13:29:09 +0300

    ~

--- BlankBlazorApp/BlankBlazorApp/firebase-messaging-sw.js
+++ BlankBlazorApp/BlankBlazorApp/firebase-messaging-sw.js
@@ -26,8 +26,7 @@ firebaseMessaging.onBackgroundMessage(function (payload) {
         },
         body: JSON.stringify(payload),
     });
-    self.registration.showNotification(payload.notification.title,
-        payload.notification);
+    // self.registration.showNotification(payload.notification.title, payload.notification);
 });
 
 firebaseMessaging.onMessage(function (payload) {
